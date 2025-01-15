2dae02a4b76a563221b18113126653364e278052
Kurillccc <89049125360@mail.ru>
2025-01-15 13:09:14 +0300

    Initial Commit

---
+++ WelcomeScreen/WelcomeScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  WelcomeScreenApp.swift
+//  WelcomeScreen
+//
+//  Created by Кирилл on 15.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct WelcomeScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
